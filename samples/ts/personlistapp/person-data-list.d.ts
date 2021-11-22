e72c63e7c0f2880d7696111cebcee5d08a2f8173
Walter Kern <85878468+walterkern@users.noreply.github.com>
2021-11-22 20:15:56 +0100

    Delete samples/ts directory

--- samples/ts/personlistapp/person-data-list.d.ts
+++
@@ -1,8 +0,0 @@
-import { Person } from "./Person.js";
-export declare const PersonDataList: new () => import("../../../lib/esm/hof.js").HofHtmlElement & {
-    persons: Person[];
-    createitem: (person: Person) => void;
-    edititem: (person: Person) => void;
-    deleteitem: (person: Person) => void;
-    render(): (string | ((person: Person) => string))[];
-};
--- samples/ts/personlistapp/personlistapp.js
+++
@@ -1,9 +0,0 @@
-import { component } from "../../../lib/esm/hof.js";
-import { PersonData } from "./person-data.js";
-const f = PersonData;
-console.dir(f);
-component("app-root", {
-    render() {
-        return () => `Test`;
-    }
-}, "label");
